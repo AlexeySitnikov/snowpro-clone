bc5741110c838621537e2173ea58b93aebeb61ea
AlexeySitnikov <alexey.sitnikov@bk.ru>
2023-04-19 15:57:30 +0300

    commit on 19.04.2023

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,28 @@
-import './style.scss'
+import './header_style.scss'
 import logo from '../../utils/image/logo1.png'
 
 export function Header() {
   function updateScroll() {
     if (window.scrollY > 0) {
       document.querySelector('header').classList.add('header__scrolled')
+      // console.log((document.querySelector('header').className))
     } else {
       document.querySelector('header').classList.remove('header__scrolled')
+      // console.log((document.querySelector('header').className))
     }
   }
   window.addEventListener('scroll', updateScroll)
+
   return (
     <header>
       <img className="header__logo" src={`${logo}`} alt="logo" />
       <div className="header__controls">
-        <a href="#form">Контакты</a>
-        <button type="button" href="#">Заказать</button>
+        <div className="header__contacts">
+          <a href="#form">Контакты</a>
+        </div>
+        <div className="header__makeOrder">
+          <button type="button" href="#">Заказать</button>
+        </div>
       </div>
     </header>
   )
